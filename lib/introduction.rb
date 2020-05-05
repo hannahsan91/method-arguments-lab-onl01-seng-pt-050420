778322a697ab17ae6e4452dec2dd18c6aa96ea76
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-05 01:28:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 # lib/introduction.rb
 
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{Ruby.rb}."
+def introduction_with_language("Josh", "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
